7a25998007a355d8831c500b51c7e0fd1fba1feb
Erickcufe <erickcufe94@gmail.com>
2021-04-30 17:43:33 -0500

    .

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+pef_2021 <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/Bases_de_datos_presupuesto/CSV/PEF_2021.csv")
+transversales_2021 <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/Bases_de_datos_presupuesto/CSV/anexos_transversales_ppef2021.csv")
+
+
+usethis::use_data(pef_2021, overwrite = TRUE)
+usethis::use_data(transversales_2021, overwrite = TRUE)
