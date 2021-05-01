62af541acec8c67e17f7a4b5ba8dad04191e0285
Erickcufe <erickcufe94@gmail.com>
2021-05-01 11:43:00 -0500

    .

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,8 +4,10 @@ pef_2021 <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/mo
 transversales_2021 <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/Bases_de_datos_presupuesto/CSV/anexos_transversales_ppef2021.csv")
 aspectos_susceptibles_mejora <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/Programas/sed/aspectos_susceptibles_mejora.csv")
 evaluaciones <- vroom::vroom("https://www.transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/Programas/sed/evaluaciones.csv")
+anexos_transversales_2021 <- readxl::read_xlsx("Anexos_Transversales_PEF_2021.xlsx")
 
 usethis::use_data(pef_2021, overwrite = TRUE)
 usethis::use_data(transversales_2021, overwrite = TRUE)
 usethis::use_data(aspectos_susceptibles_mejora, overwrite = TRUE)
 usethis::use_data(evaluaciones, overwrite = TRUE)
+usethis::use_data(anexos_transversales_2021, overwrite = TRUE)
